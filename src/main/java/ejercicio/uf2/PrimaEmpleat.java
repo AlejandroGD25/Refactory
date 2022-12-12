a25729ffb685708423f041c58afd6852f5b4996e
Alejandro <100007465.joan23@fje.edu>
2022-12-12 17:26:06 +0100

    Hola master

--- src/main/java/ejercicio/uf2/PrimaEmpleat.java
+++ src/main/java/ejercicio/uf2/PrimaEmpleat.java
@@ -6,31 +6,40 @@ public class PrimaEmpleat {
     private String nom;
     private String directiu;
     private int antiguitat;
+    final int Antiguetat = 12;
+    final int maxAntiguetat = 1000;
+    public int p = 0;
 
-	public static void main(String[] args) {
-            PrimaEmpleat e = new PrimaEmpleat("Elvira", "S", 11);
-
-            int p = 0;
-            if (e.antiguitat >= 0 && e.antiguitat < 1000) {
-                if (e.directiu == "S") {
-                    if (e.antiguitat > 12)
-                        p = 600;
-                    else {
-                        p = 400;
+	public void AdjuntarPrima(PrimaEmpleat e) {
+            final int primaDirectiuAntic = 600;
+            final int primaDirectiuNou = 400;
+            final int primaEmpleatAntic = 150;
+            final int primaEmpleatNou = 100;
+            if(e.antiguitat >= 0 && e.antiguitat < maxAntiguetat)
+                if(e.directiu == "S"){
+                    p = primaDirectiuAntic;
+                } else {
+                    p = primaDirectiuNou;
+                } else if(e.directiu == "N"){
+                    if(e.antiguitat > Antiguetat){
+                        p = primaEmpleatAntic;
+                    } else {
+                        p = primaEmpleatNou;
                     }
-                } else if (e.directiu == "N") {
-                    if (e.antiguitat > 12)
-                        p = 150;
-                    else
-                        p = 100;
-                } else
-                System.out.println("El codi del càrrec ha de ser 'S' o 'N'");
-                if (p!=0)
-                System.out.println("La prima que li correspon a " + e.nom + " és de " + p + " Euros");
-            } else
-                System.out.println("La antiguitat ha de ser un nombre entre 0 i 999");
+                } else {
+                    System.out.println("El codi del càrrec ha de ser 'S' o 'N'");
+                }
+                if(p != 0){
+                    System.out.println("La prima que li correspon a " + e.nom + " és de " + p + " Euros");
+                } else {
+                    System.out.println("La antiguitat ha de ser un nombre entre 0 i 999");
+                }
         }
-
+        
+        public static void main(String[] args) {
+            PrimaEmpleat e = new PrimaEmpleat("Elvira", "S", 11);
+        }
+        
         public PrimaEmpleat(String nome, String dire, int ante) {
             this.nom = nome;
             this.directiu = dire;
